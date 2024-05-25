c8ff98a76430ce64b1ff3e7f0fc7fa288c944bd0
HAYBISH10 <hassanissackmohamed172@gmail.com>
2024-05-25 15:33:55 +0300

    file create

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const word = document.getElementById("list")
 
 document.getElementById("button-addon2").addEventListener("click",function(){
    if (input.value === "") {
-    alert("you input is empty")
+    alert("your input is empty")
 
    }else{
         const list=document.createElement("li")
@@ -15,5 +15,32 @@ document.getElementById("button-addon2").addEventListener("click",function(){
     input.value= ""
 })
 word.addEventListener ("click", function(e){
-    e.target.remove();
+    // e.target.remove();
+    e.target.style.textDecoration="line-through"
+    save()
 })
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "Enter"){
+        if(input.value===""){
+            alert("i know you press me but nothing you input")
+        }else{
+        const list=document.createElement("li")
+        list.textContent=input.value;
+        list.classList.add("list-group-item","category-tag")
+        word.appendChild(list)
+       
+    }
+    input.value= ""
+    save()
+}
+ })
+
+ function save(){
+    localStorage.setItem("data", word.innerHTML)
+ }
+
+ function read(){
+    word.innerHTML=localStorage.getItem("data")
+ }
+ read()
